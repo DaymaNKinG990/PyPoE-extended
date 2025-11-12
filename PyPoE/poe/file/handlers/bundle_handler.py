09d5c726a2feee7ad6ab53ab646d0526ba825829
Ravil Shakerov <xellaopromaster@yandex.ru>
2025-11-12 23:06:04 +0300

    fix: убрать неиспользуемый импорт Bundle

--- PyPoE/poe/file/handlers/bundle_handler.py
+++ PyPoE/poe/file/handlers/bundle_handler.py
@@ -7,7 +7,6 @@ This module provides BundleHandler for processing Bundle files.
 from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
-    from PyPoE.poe.file.bundle import Bundle
     from PyPoE.poe.file.shared import AbstractFileReadOnly
 
 from PyPoE.poe.file.handlers.base import FileHandler
